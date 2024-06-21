cdbf81975378c1d0f9c95a182d7ba65ed110533c
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-06-21 13:42:42 +0530

    added all the scripts file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -43,4 +43,9 @@ async function verify(contractAddress, args) {
   }
 }
 
-// Hardhat recommends this pat
+// Hardhat recommends this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
